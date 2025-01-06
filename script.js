ae1fe91a92ba81bb5ec13f063d4aa426c2a0c038
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-07 03:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let tbody = document.getElementById('output');
 
-tbody.innerHTML = `<tr>
+tbody.innerHTML = `<tr id="loading">
 <td colspan="2">Loading...</td>
 </tr>`
 
@@ -21,19 +21,19 @@ let promise3 = new Promise((resolve , reject)=>{
 	   setTimeout(()=>{
 		    resolve(3)
 	   },3000)
- })  
+ })   
 
 
 Promise.all([promise1, promise2, promise3]).then((values) => {
   // clear the loading text
-  tbody.innerHTML = '';
+  document.getElementById('loading').remove();
 	// iterate over the values array and create a row for each promise
   for(let i = 0; i < values.length; i++){
     tbody.innerHTML += `<tr>
-        <td>Promise ${i+1}</td>
+        <td>Promise ${i+1}</td> 
         <td>${values[i]}</td>
         </tr>`
-  }
+  } 
 // calculate the total time
   let totalTime = values.reduce((a, b) => a + b, 0);
     // add the total time row
